a184b01c9a73850d56db0e3ddc7f29ffb1faaef2
wsk <1138540095@qq.com>
2025-06-14 00:37:08 +0800

    在线状态显示

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 
-onMounted(() => {
-  console.log('[App.vue] 注册 WebSocket 监听')
-
-  window.api.onWSStatus((status) => {
-    console.log('[UI] WebSocket 状态变化:', status)
-  })
-
-  window.api.onWSMessage((msg) => {
-    alert(`[WebSocket] 收到消息: ${JSON.stringify(msg)}`)
-  })
-})
 </script>
 
 <template>
