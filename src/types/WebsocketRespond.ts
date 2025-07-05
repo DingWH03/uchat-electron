726c856bac98d20470478a18e21c50875b61476e
DingVero <2521248869@qq.com>
2025-07-05 11:25:01 +0800

    同步后端修改了ServerMessage的类型，增加message_id

--- src/types/WebsocketRespond.ts
+++ src/types/WebsocketRespond.ts
@@ -4,12 +4,15 @@ import { z } from 'zod'
 export const ServerMessageSchema = z.discriminatedUnion('type', [
   z.object({
     type: z.literal('SendMessage'),
+    message_id: z.number(),
     sender: z.number(),
+    receiver: z.number(),
     message: z.string(),
     timestamp: z.number()
   }),
   z.object({
     type: z.literal('SendGroupMessage'),
+    message_id: z.number(),
     sender: z.number(),
     group_id: z.number(),
     message: z.string(),
